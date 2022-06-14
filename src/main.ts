c1fe2e1b4848b56d6a0de739c943bb4d97c19278
senjiro224 <gantaiyeze1@gmail.com>
2022-06-14 09:01:53 +0900

    とりあえずスターと画面とリザルト画面らしきものを作り、画面が遷移できるようにしました。

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,43 @@ const Hackathon = () => {
   const mapController = new MapController(mapData);
   const playerController = new PlayerController(playerData, mapData);
 
+  //keydownイベントが起こったときの画面遷移
+  //諸々の処理はこれから書きます。
+  window.addEventListener('keydown',(e) => {
+    switch(settings.mode){
+      case 'title':{
+        switch(e.key){
+          case ' ':
+            settings.mode='game';
+            break;
+          case '1':
+            break;
+          case '2':
+            break;
+          case '3':
+            break;
+          default:
+            break;
+        }
+        break;
+      }
+      case 'game':
+        break;
+      case 'result':{
+        switch(e.key){
+          case ' ':
+            settings.mode='title';
+            break;
+          default:
+            break;
+        }
+        break;
+      }
+
+    }
+    }
+  )
+
   const tick = () => {
     requestAnimationFrame(tick);
 
@@ -27,6 +64,17 @@ const Hackathon = () => {
     switch (settings.mode) {
       case 'title':
         // title rendering
+        const COL_TEXT="rgba(0,0,0,1)";//テキストの色
+
+        ctx.fillStyle=COL_TEXT;
+        ctx.font='32px sans-serif';
+        ctx.fillText('Game Title',220,50);
+        ctx.font='16px sans-serif';
+        ctx.fillText('Start -> Press space key',220,290);
+        ctx.fillText('Tutorial -> Press 1 key',220,320);
+        ctx.fillText('Config -> Press 2 key',220,350);
+        ctx.fillText('Character select -> Press 3 key',220,380);
+
         break;
       case 'game':
         mapController.render(ctx);
@@ -34,6 +82,9 @@ const Hackathon = () => {
         break;
       case 'result':
         // result rendering
+        ctx.font='32px sans-serif';
+        ctx.fillText('Your got <score> points!!',100,100);
+        ctx.fillText('Press space to back to Start',100,200);
         break;
       default:
         throw new Error('Unknown mode');
