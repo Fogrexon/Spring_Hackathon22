3046c938b849d3055b414793cf271aae622e3f8c
HaRo <monaka150913@gmail.com>
2022-06-18 19:42:16 +0900

    ghostmove

--- src/type/ghostType.ts
+++ src/type/ghostType.ts
@@ -3,62 +3,63 @@ import { MapData } from '../data/mapData';
 import { PlayerData } from '../data/playerData';
 import { checkCollisionWall } from '../mover/ghostMover';
 
-export const ghostType = (tekito: GhostData, playerData: PlayerData, mapData: MapData) => { // 壁の向きにいかないように
-  const walldirect = (tekito: GhostData, mapData: MapData) =>{
-    if (!checkCollisionWall(tekito.gx, tekito.gy - 1, mapData)) { // どの壁に向かうとき？→上。壁にいったら距離を10000にする
-      tekito.up = 10000;
-    } else if (!checkCollisionWall(tekito.gx, tekito.gy + 1, mapData)) { // 下
-      tekito.down = 10000;
-    } else if (!checkCollisionWall(tekito.gx - 1, tekito.gy, mapData)) { // left
-      tekito.left = 10000;
-    } else (!checkCollisionWall(tekito.gx + 1, tekito.gy, mapData)) { // riget
-      tekito.right = 10000;
-    }
-  };
-  const backdirect = (tekito: GhostData) =>{ // 来た道に戻らないように
-    if (tekito.gdirect === 'gUp') { // 今来たマスでの距離を9999にする
-      tekito.down = 9999;
-    } else if (tekito.gdirect === 'gDown') {
-      tekito.up = 9999;
-    } else if (tekito.gdirect === 'gLeft') {
-      tekito.right = 9999;
-    } else (tekito.gdirect === 'gRight') {
-      tekito.left = 9999;
-    }
-  };
-  
+interface AroundValue {
+    left: number;
+    right: number;
+    up: number;
+    down: number;
+}
+
+export const ghostType = (tekito: GhostData, playerData: PlayerData, mapData: MapData, arroundvalue: AroundValue) => { // 壁の向きにいかないよ
+    const walldirect = (aroundvalue: AroundValue, tekito: GhostData, mapData: MapData) =>{
+        if (!checkCollisionWall(tekito.gx, tekito.gy - 1, mapData)) { // どの壁に向かうとき？→上。壁にいったら距離を10000にする
+          aroundvalue.up = 10000;
+        } else if (!checkCollisionWall(tekito.gx, tekito.gy + 1, mapData)) { // 下
+          aroundvalue.down = 10000;
+        } else if (!checkCollisionWall(tekito.gx - 1, tekito.gy, mapData)) { // left
+          aroundvalue.left = 10000;
+        } else (!checkCollisionWall(tekito.gx + 1, tekito.gy, mapData)) { // right
+          aroundvalue.right = 10000;
+        }
+    };
+    const backdirect = (aroundvalue: AroundValue, tekito: GhostData) =>{ // 来た道に戻らないように
+        if (tekito.gdirect === 'gUp') { // 今来たマスでの距離を9999にする
+          aroundvalue.down = 9999;
+        } else if (tekito.gdirect === 'gDown') {
+          aroundvalue.up = 9999;
+        } else if (tekito.gdirect === 'gLeft') {
+          aroundvalue.right = 9999;
+        } else (tekito.gdirect === 'gRight') {
+          aroundvalue.left = 9999;
+        }
+    };
+    const checkAroundValue = () => { // 壁と来た道の値を無限大にする
+        const aroundvalue: AroundValue = {
+            left: Math.floor(Math.random() * (-100)),
+            right: Math.floor(Math.random() * (-100)),
+            up: Math.floor(Math.random() * (-100)),
+            down: Math.floor(Math.random() * (-100)), // 負の値からランダムな初期距離を代入
+        };
+        walldirect (aroundvalue, tekito, mapData);
+        backdirect (aroundvalue, tekito);
+    };
+    const MinDirectJudge = (distance: AroundValue, ghostData: GhostData) => {  // どれが最短か
+        if (Math.min(distance.up,distance.down,distance.left,distance.right) === distance.up) {
+            ghostData.gdirect ='gUp'
+        }else if (Math.min(distance.up,distance.down,distance.left,distance.right) === distance.down) {
+            ghostData.gdirect ='gDown'
+        }else if (Math.min(distance.up,distance.down,distance.left,distance.right) === distance.left) {
+            ghostData.gdirect ='gLeft'
+        }else (Math.min(distance.up,distance.down,distance.left,distance.right) === distance.right) {
+            ghostData.gdirect ='gRight'
+        }
+    };
+
   switch (tekito.gtype) {
     case 'random': {       // GhostDataがghostData1のとき
-      walldirect(tekito, mapData)
-      backdirect(tekito)
+      checkAroundValue();
       // 距離の最小値を取ろう。距離は考えてないので追跡はしない
-      if (Math.min(tekito.up,tekito.down,tekito.left,tekito.right) === tekito.up) {
-        tekito.gdirect ='gUp'
-      }else if (Math.min(tekito.up,tekito.down,tekito.left,tekito.right) === tekito.down) {
-          tekito.gdirect ='gDown'
-      }else if (Math.min(tekito.up,tekito.down,tekito.left,tekito.right) === tekito.left) {
-          tekito.gdirect ='gLeft'
-      }else (Math.min(tekito.up,tekito.down,tekito.left,tekito.right) === tekito.right) {
-          tekito.gdirect ='gRight'
-      }
-      tekito.up =0;
-      tekito.down = 0;
-      tekito.left = 0;
-      tekito.right = 0;
-
-      const random = Math.floor(Math.random() * 4);
-      // ランダム方向決定。壁がある方向と来た方向に高い点（距離）を与える
-
-      if (random === 0) {
-        tekito.gdirect = 'gUp';
-      } else if (random === 1) {
-        tekito.gdirect = 'gDown';
-      } else if (random === 2) {
-        tekito.gdirect = 'gLeft';
-      } else {
-        tekito.gdirect = 'gRight';
-      }
-      break;
+      MinDirectJudge(arroundvalue,tekito);
     }
     case 'chase': { // 追跡型方向決定.x,y座標の違いを比較して大きい方に動かす.
       const dx = tekito.gx - playerData.x;
