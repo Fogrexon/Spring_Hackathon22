0a1d4dad763fd4acef740d918fc08cfbb29741a3
shihu <methacrylicacid86@gmail.com>
2022-06-19 03:22:18 +0900

    追加

--- src/controller/stageController.ts
+++ src/controller/stageController.ts
@@ -27,7 +27,7 @@ export const moveNextMap = () => {
 export const getCurrentMap = () => stageData.maps[stageData.current];
 
 export const stageReset = () => {
-  for (let i = 0; i < stageData.maps.length - 1; i += 1) {
+  for (let i = 0; i < stageData.maps.length ; i += 1) {
     stageData.maps[i].exist = [true, true, true, true];
   }
 };
